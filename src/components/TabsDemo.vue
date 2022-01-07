2addad1a103cb5321584d98b45ee83987fa13a66
Johnsean <956407921@qq.com>
2022-01-08 01:02:45 +0800

    Tabs内部元素自查

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,5 +1,18 @@
 <template>
-    <div>
-        tabs 示例
-    </div>
+    <h2>Tabs示例</h2>
+    <Tabs>
+        <Tab title="学习">学习</Tab>
+        <Tab title="吃饭">吃饭</Tab>
+        <Tab title="睡觉">睡觉</Tab>
+    </Tabs>
 </template>
+
+<script lang="ts">
+import Tabs from '../lib/Tabs.vue';
+import Tab from '../lib/Tab.vue';
+  
+export default {
+  name: 'TabsDemo',
+  components: {Tabs, Tab},
+};
+</script>
---
+++ src/lib/Tabs.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <slot></slot>
+    </div>
+</template>
+<script lang="ts">
+import Tab from './Tab.vue'
+
+export default{
+    name: 'Tabs',
+    props:{
+        titles:Object
+    },
+    setup(props,context){
+        const tagList = context.slots.default()
+        console.log(context.slots.default())
+        tagList.forEach((tag)=>{
+            if (tag.type !== Tab){
+                throw new Error('Tabs 的内部必须是 Tab')
+            }
+        })
+    }
+}
+</script>
