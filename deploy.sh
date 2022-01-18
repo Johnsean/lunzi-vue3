2fe0da357b052fab4d9d3de67ad94f8585386e8f
Johnsean <956407921@qq.com>
2022-01-18 23:53:25 +0800

    deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+# rm -rf dist &&
+# yarn build &&
+# cd dist &&
+# git init &&
+# git add . &&
+# git commit -m "update" &&
+# git branch -M master &&
+# git remote add github  git@github.com:Johnsean/lunzi-vue3.git &&
+# git push -f -u github master &&
+# git remote add origin  https://gitee.com/Jonhsean/lunzi-vue3.git &&
+# git push -f -u origin master &&
+# cd -
+# echo https://gitee.com/Jonhsean/lunzi-vue3/index.htm
