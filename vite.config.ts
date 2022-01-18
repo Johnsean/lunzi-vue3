82ee7ee3bf55217d932d68a95b0f1f1731a43d8e
Johnsean <956407921@qq.com>
2022-01-18 16:58:46 +0800

    代码优化-使用 component 标签代替

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default {
     demo: (options) => {
       const { code, path } = options
       const file = fs.readFileSync(path).toString() //异步读取文件内容并转为 String类型
-      const parsed = baseParse(file).children.find(n => n.tag === "demo")  //将读取到的自定义块 渲染为AST
+      const parsed = baseParse(file).children.find(n => n.tag === "demo")  //将读取到的自定义块 渲染为AST ，并找demo标签
       const title = parsed.children[0].content //读取自定义块中文本内容
       const main = file.split(parsed.loc.source).join("").trim() //将读取文件中自定义块切分，并转为 String类型
       return `export default function (Component) {
