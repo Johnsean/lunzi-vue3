ae375dfa96834f41de6c871fecf6a676ba9bae8f
Johnsean <956407921@qq.com>
2022-01-19 15:41:59 +0800

    ---demo包名优化

--- src/components/TabsPage/TabsDemo.vue
+++ src/components/TabsPage/TabsDemo.vue
@@ -6,7 +6,7 @@
       使用<span>v-model</span>绑定一个<span>Boolean</span>类型的变量到<span>Tabs</span>组件,指定默认<span>Tab</span>
     </p>
   </section>
-  <Demo :component="Tabs1Demo"/>
+  <Demo :component="TabsNormal"/>
 
   <section class="second-title">
     <h2>支持disabled属性</h2>
@@ -19,14 +19,14 @@
 
 <script lang="ts">
 import Demo from '../Demo.vue'
-import Tabs1Demo from '../TabsPage/TabsDemo/Tabs1.demo.vue'
+import TabsNormal from '../TabsPage/TabsDemo/TabsNormal.vue'
 import TabsDisabled from "./TabsDemo/TabsDisabled.vue"
 
 export default {
   name: 'TabsDemo',
   components: { Demo },
   setup(){
-    return { Tabs1Demo,TabsDisabled, }
+    return { TabsNormal,TabsDisabled, }
   }
 };
 </script>
