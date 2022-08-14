06c9ace52cc2444577a2a3136ead1080bf4a6ceb
Johnsean <956407921@qq.com>
2022-08-14 22:58:49 +0800

    显示隐藏子节点

--- src/lib/Tree.vue
+++ src/lib/Tree.vue
@@ -1,26 +1,36 @@
 <template>
-    <div class="trees">
-        <div class="list-item" v-for="(item, index) in list" :key="index">
-            <div class="item-name">
-                <span>{{item.label}}</span>
-            </div>
-            <div v-if="item.children" class="children-item">
-                <Tree :list="item.children" />
-            </div>
-        </div>
+  <div class="trees">
+    <div class="list-item" v-for="(item, index) in myList" :key="index">
+      <div class="item-name" @click="item.isShow = !item.isShow">
+        <span>{{ item.label }}</span>
+      </div>
+      <Tree
+        v-if="item.children"
+        :data="item.children"
+        style="margin-left: 20px"
+        class="children-item"
+        v-show="item.isShow"
+      />
     </div>
+  </div>
 </template>
 
 <script>
 import { computed, ref } from "vue";
 export default {
   name: "tree",
-  props:{
-      list:{
-          type:Array,
-          default:[]
-      }
+  props: {
+    data: {
+      type: Array,
+      required: true,
+      default: [],
+    },
   },
-
-}
+  setup(props, context) {
+    const myList = computed(() =>
+      props.data.map((r) => Object.assign(r, { isShow: false }))
+    );
+    return { myList };
+  },
+};
 </script>
